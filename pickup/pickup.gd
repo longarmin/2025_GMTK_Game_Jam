fb0896351f27e7f2710c75d1cbc6032ac66fc750
Sebastian <sthon@hotmail.de>
2025-08-01 00:12:28 +0200

    Energy-Pickups implementiert.

---
+++ pickup/pickup.gd
@@ -0,0 +1,11 @@
+class_name Pickup extends Area2D
+
+var power_gain := 10.0
+
+func _ready() -> void:
+	body_entered.connect(func(body: Node2D) -> void:
+		if body is Player:
+			print(body.energy)
+			body.energy += power_gain
+			queue_free()
+	)
