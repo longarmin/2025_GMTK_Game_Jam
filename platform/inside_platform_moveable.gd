6493f5f6f9cfebae90d114dadeb0da42eac6d42c
Sebastian <sthon@hotmail.de>
2025-08-01 23:00:15 +0200

    Spaces zu Tabs in der Datei

--- platform/inside_platform_moveable.gd
+++ platform/inside_platform_moveable.gd
@@ -6,22 +6,22 @@ class_name InsidePlatformMoveable extends Platform
 var start_position: Vector2
 
 func _ready() -> void:
-    super ()
-    if Engine.is_editor_hint():
-        return
-    start_position = global_position
-    print(global_position)
-    # Use set_deferred to ensure the change happens after the physics engine initialization
-    set_deferred("global_position", map_centrum)
-    print(global_position)
-    var difference_position = start_position - map_centrum
-    collision_shape_2d.global_position += difference_position
-    sprite.global_position += difference_position
+	super ()
+	if Engine.is_editor_hint():
+		return
+	start_position = global_position
+	print(global_position)
+	# Use set_deferred to ensure the change happens after the physics engine initialization
+	set_deferred("global_position", map_centrum)
+	print(global_position)
+	var difference_position = start_position - map_centrum
+	collision_shape_2d.global_position += difference_position
+	sprite.global_position += difference_position
 
 func set_height(value: float) -> void:
-    height = value
-    if shape == null:
-        return
-    shape.size.y = height
-    sprite.position.y = - height / 2.0
-    sprite.size.y = height
+	height = value
+	if shape == null:
+		return
+	shape.size.y = height
+	sprite.position.y = - height / 2.0
+	sprite.size.y = height
