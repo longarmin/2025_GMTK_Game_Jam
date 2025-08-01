9bb466b7c856d362080a552b996bedd5f901beec
Sebastian <sthon@hotmail.de>
2025-08-01 14:04:51 +0200

    Erster Entwurf für Menüs und Energie-Anzeige.

---
+++ ui/end_menu.gd
@@ -0,0 +1,12 @@
+class_name EndMenu extends Control
+
+@onready var main_menu_button: Button = %MainMenuButton
+@onready var exit_game_button: Button = %ExitGameButton
+
+func _ready() -> void:
+    main_menu_button.pressed.connect(func() -> void:
+        GameManager.change_state(GameManager.GameState.MAIN_MENU)
+    )
+    exit_game_button.pressed.connect(func() -> void:
+        get_tree().quit()
+    )
---
+++ ui/main_menu.gd
@@ -0,0 +1,19 @@
+class_name MainMenu extends Control
+
+@onready var start_button: Button = %StartButton
+@onready var controls_button: Button = %ControlsButton
+@onready var controls_exit_button: Button = %ControlsExitButtonButton
+@onready var controls_container: MarginContainer = %ControlsContainer
+
+func _ready() -> void:
+    controls_container.hide()
+
+    start_button.pressed.connect(func() -> void:
+        GameManager.change_state(GameManager.GameState.GAME)
+    )
+    controls_button.pressed.connect(func() -> void:
+        controls_container.show()
+    )
+    controls_exit_button.pressed.connect(func() -> void:
+        controls_container.hide()
+    )
